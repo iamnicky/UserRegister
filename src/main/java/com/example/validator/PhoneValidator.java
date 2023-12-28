ad8fe74609e107090c289380ad9dce5ca71dc034
iamnicky <2020ugee067@nitjsr.ac.in>
2023-12-29 01:26:36 +0530

    UC4

---
+++ src/main/java/com/example/validator/PhoneValidator.java
@@ -0,0 +1,11 @@
+package com.example.validator;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class PhoneValidator {
+    public static boolean isPhoneValid(String input) {
+        Pattern pattern = Pattern.compile("[0-9]{2}\\s[0-9]{10}");
+        return pattern.matcher(input).matches();
+    }
+}
